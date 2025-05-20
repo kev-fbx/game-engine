8798a2d54b0ed735de166f8a6a9db02eb09a95f0
kev-fbx <ktran041203@gmail.com>
2025-05-20 23:16:40 +1000

    commit

---
+++ GameEngine/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Engine {
+
+	void Print() {
+		printf("Hello from Engine!\n");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,7 @@
+namespace Engine {
+	__declspec(dllexport) void Print();
+}
+
+void main() {
+	Engine::Print();
+}
